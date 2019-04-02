5c0c165b004171cd5d67f598910ab255aba12bbb
alainsfeir1 <39865417+alainsfeir1@users.noreply.github.com>
2019-04-02 00:09:53 -0400

    Add files via upload

---
+++ CalculatorCMPLX/frmMain.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace Lab7_Alain_Sfeir
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtnum1X = new System.Windows.Forms.TextBox();
+            this.txtnum1y = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtnum2x = new System.Windows.Forms.TextBox();
+            this.txtnum2y = new System.Windows.Forms.TextBox();
+            this.btnAdduser = new System.Windows.Forms.Button();
+            this.btnSubtractuser = new System.Windows.Forms.Button();
+            this.btnMultiplyuser = new System.Windows.Forms.Button();
+            this.btnDivideuser = new System.Windows.Forms.Button();
+            this.btnAddtxt = new System.Windows.Forms.Button();
+            this.btnSubtracttxt = new System.Windows.Forms.Button();
+            this.btnMultiplytxt = new System.Windows.Forms.Button();
+            this.btnDividetxt = new System.Windows.Forms.Button();
+            this.lstAnswertxt = new System.Windows.Forms.ListBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input Complex Numbers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(473, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Answer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(100, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "+";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(166, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "i";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(71, 312);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(230, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Calculations Button: Using input file";
+            // 
+            // txtnum1X
+            // 
+            this.txtnum1X.Location = new System.Drawing.Point(60, 103);
+            this.txtnum1X.Name = "txtnum1X";
+            this.txtnum1X.Size = new System.Drawing.Size(35, 22);
+            this.txtnum1X.TabIndex = 5;
+            // 
+            // txtnum1y
+            // 
+            this.txtnum1y.Location = new System.Drawing.Point(123, 103);
+            this.txtnum1y.Name = "txtnum1y";
+            this.txtnum1y.Size = new System.Drawing.Size(37, 22);
+            this.txtnum1y.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(291, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 32);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "+";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(364, 98);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 32);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "i";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label8.Location = new System.Drawing.Point(71, 201);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(237, 17);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Calculation Button: Using user input.";
+            // 
+            // txtnum2x
+            // 
+            this.txtnum2x.Location = new System.Drawing.Point(249, 103);
+            this.txtnum2x.Name = "txtnum2x";
+            this.txtnum2x.Size = new System.Drawing.Size(36, 22);
+            this.txtnum2x.TabIndex = 10;
+            // 
+            // txtnum2y
+            // 
+            this.txtnum2y.Location = new System.Drawing.Point(324, 103);
+            this.txtnum2y.Name = "txtnum2y";
+            this.txtnum2y.Size = new System.Drawing.Size(34, 22);
+            this.txtnum2y.TabIndex = 11;
+            // 
+            // btnAdduser
+            // 
+            this.btnAdduser.Location = new System.Drawing.Point(74, 249);
+            this.btnAdduser.Name = "btnAdduser";
+            this.btnAdduser.Size = new System.Drawing.Size(86, 30);
+            this.btnAdduser.TabIndex = 12;
+            this.btnAdduser.Text = "Add";
+            this.btnAdduser.UseVisualStyleBackColor = true;
+            this.btnAdduser.Click += new System.EventHandler(this.btnAdduser_Click);
+            // 
+            // btnSubtractuser
+            // 
+            this.btnSubtractuser.Location = new System.Drawing.Point(183, 248);
+            this.btnSubtractuser.Name = "btnSubtractuser";
+            this.btnSubtractuser.Size = new System.Drawing.Size(82, 31);
+            this.btnSubtractuser.TabIndex = 13;
+            this.btnSubtractuser.Text = "Subtract";
+            this.btnSubtractuser.UseVisualStyleBackColor = true;
+            this.btnSubtractuser.Click += new System.EventHandler(this.btnSubtractuser_Click);
+            // 
+            // btnMultiplyuser
+            // 
+            this.btnMultiplyuser.Location = new System.Drawing.Point(297, 248);
+            this.btnMultiplyuser.Name = "btnMultiplyuser";
+            this.btnMultiplyuser.Size = new System.Drawing.Size(78, 31);
+            this.btnMultiplyuser.TabIndex = 14;
+            this.btnMultiplyuser.Text = "Multiply";
+            this.btnMultiplyuser.UseVisualStyleBackColor = true;
+            this.btnMultiplyuser.Click += new System.EventHandler(this.btnMultiplyuser_Click);
+            // 
+            // btnDivideuser
+            // 
+            this.btnDivideuser.Location = new System.Drawing.Point(408, 248);
+            this.btnDivideuser.Name = "btnDivideuser";
+            this.btnDivideuser.Size = new System.Drawing.Size(80, 31);
+            this.btnDivideuser.TabIndex = 15;
+            this.btnDivideuser.Text = "Divide";
+            this.btnDivideuser.UseVisualStyleBackColor = true;
+            this.btnDivideuser.Click += new System.EventHandler(this.btnDivideuser_Click);
+            // 
+            // btnAddtxt
+            // 
+            this.btnAddtxt.Location = new System.Drawing.Point(74, 358);
+            this.btnAddtxt.Name = "btnAddtxt";
+            this.btnAddtxt.Size = new System.Drawing.Size(86, 32);
+            this.btnAddtxt.TabIndex = 16;
+            this.btnAddtxt.Text = "Add";
+            this.btnAddtxt.UseVisualStyleBackColor = true;
+            this.btnAddtxt.Click += new System.EventHandler(this.btnAddtxt_Click);
+            // 
+            // btnSubtracttxt
+            // 
+            this.btnSubtracttxt.Location = new System.Drawing.Point(183, 357);
+            this.btnSubtracttxt.Name = "btnSubtracttxt";
+            this.btnSubtracttxt.Size = new System.Drawing.Size(82, 33);
+            this.btnSubtracttxt.TabIndex = 17;
+            this.btnSubtracttxt.Text = "Subtract";
+            this.btnSubtracttxt.UseVisualStyleBackColor = true;
+            this.btnSubtracttxt.Click += new System.EventHandler(this.btnSubtracttxt_Click);
+            // 
+            // btnMultiplytxt
+            // 
+            this.btnMultiplytxt.Location = new System.Drawing.Point(297, 357);
+            this.btnMultiplytxt.Name = "btnMultiplytxt";
+            this.btnMultiplytxt.Size = new System.Drawing.Size(78, 33);
+            this.btnMultiplytxt.TabIndex = 18;
+            this.btnMultiplytxt.Text = "Multiply";
+            this.btnMultiplytxt.UseVisualStyleBackColor = true;
+            this.btnMultiplytxt.Click += new System.EventHandler(this.btnMultiplytxt_Click);
+            // 
+            // btnDividetxt
+            // 
+            this.btnDividetxt.Location = new System.Drawing.Point(408, 357);
+            this.btnDividetxt.Name = "btnDividetxt";
+            this.btnDividetxt.Size = new System.Drawing.Size(80, 33);
+            this.btnDividetxt.TabIndex = 19;
+            this.btnDividetxt.Text = "Divide";
+            this.btnDividetxt.UseVisualStyleBackColor = true;
+            this.btnDividetxt.Click += new System.EventHandler(this.btnDividetxt_Click);
+            // 
+            // lstAnswertxt
+            // 
+            this.lstAnswertxt.FormattingEnabled = true;
+            this.lstAnswertxt.ItemHeight = 16;
+            this.lstAnswertxt.Location = new System.Drawing.Point(657, 56);
+            this.lstAnswertxt.Name = "lstAnswertxt";
+            this.lstAnswertxt.Size = new System.Drawing.Size(213, 340);
+            this.lstAnswertxt.TabIndex = 20;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.Location = new System.Drawing.Point(456, 106);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(132, 24);
+            this.lblAnswer.TabIndex = 21;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 454);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lstAnswertxt);
+            this.Controls.Add(this.btnDividetxt);
+            this.Controls.Add(this.btnMultiplytxt);
+            this.Controls.Add(this.btnSubtracttxt);
+            this.Controls.Add(this.btnAddtxt);
+            this.Controls.Add(this.btnDivideuser);
+            this.Controls.Add(this.btnMultiplyuser);
+            this.Controls.Add(this.btnSubtractuser);
+            this.Controls.Add(this.btnAdduser);
+            this.Controls.Add(this.txtnum2y);
+            this.Controls.Add(this.txtnum2x);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtnum1y);
+            this.Controls.Add(this.txtnum1X);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtnum1X;
+        private System.Windows.Forms.TextBox txtnum1y;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtnum2x;
+        private System.Windows.Forms.TextBox txtnum2y;
+        private System.Windows.Forms.Button btnAdduser;
+        private System.Windows.Forms.Button btnSubtractuser;
+        private System.Windows.Forms.Button btnMultiplyuser;
+        private System.Windows.Forms.Button btnDivideuser;
+        private System.Windows.Forms.Button btnAddtxt;
+        private System.Windows.Forms.Button btnSubtracttxt;
+        private System.Windows.Forms.Button btnMultiplytxt;
+        private System.Windows.Forms.Button btnDividetxt;
+        private System.Windows.Forms.ListBox lstAnswertxt;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ CalculatorCMPLX/frmMain.cs
@@ -0,0 +1,255 @@
+﻿/* Lab 7
+ SEMESTER Winter 2019 
+ Alain, Sfeir 
+ I fully understand the following statement. 
+ OU PLAGIARISM POLICY 
+ ‘All members of the academic community at Oakland are expected to practice and 
+ uphold ‘standards of academic integrity and honesty. An instructor is expected to 
+ inform and instruct students about the procedures and standards of research and 
+ documentation required of students ‘in fulfilling course work. A student is expected 
+ to follow such instructions and be sure the rules ‘and procedures are understood in 
+ order to avoid inadvertent misrepresentation of his/her work. ‘Students must assume that 
+ individual (unaided) work on exams and lab reports and documentation ‘of sources is 
+ expected unless the instructor specifically says that is not necessary. 
+ 
+‘The following definitions are some examples of academic dishonesty:  
+‘Plagiarizing from work of others. Plagiarism is using someone else's work or ideas without ‘giving the 
+other person credit; by doing this, a student is, in effect, claiming credit for 
+‘someone else's thinking. Whether the student has read or heard the information she/he 
+uses, ‘the student must document the source of information. When dealing with written sources,
+‘a clear distinction would be made between quotations (which reproduce information 
+from ‘the source word-for-word within quotation marks) and paraphrases (which digest 
+the ‘source information and produce it in the student's own words). Both direct quotations
+and  ‘paraphrases must be documented. Just because a student rephrases, 
+condenses or selects ‘from another person's work, the ideas are still the other person's,
+and failure to give ‘credit constitutes misrepresentation of the student's actual work 
+and plagiarism of ‘another's ideas. Naturally, buying a paper and handing it in as one's
+own work is ‘plagiarism. 
+‘Cheating on lab reports falsifying data or submitting data not based on student's own work. */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab7_Alain_Sfeir
+{
+    public partial class frmMain : Form
+    {   //create a structure
+        public struct Numbers
+        {
+            //first complex number
+            public int numX;
+            public int numY;
+            
+        }
+        //create a list
+        Numbers[] mylist = new Numbers[100];
+        public frmMain()
+        {
+            string temp;
+            string[] detail;
+            int idx = 0;
+            InitializeComponent();
+            StreamReader myFile;
+            myFile = File.OpenText("Input.txt");
+
+            while (!myFile.EndOfStream)
+            {
+                temp = myFile.ReadLine();
+    
+                detail = temp.Split(',');
+                //put the split 
+                int.TryParse(detail[0], out mylist[idx].numX);
+                int.TryParse(detail[1], out mylist[idx].numY);
+                
+
+                idx++;
+            }
+        }
+   
+        
+        
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Method to calculate the addition
+        public string Addition(int real1, int imaginary1, int real2, int imaginary2 )
+        {
+            string result;
+            result = ComplexMath.MathFunctions.Add(real1, imaginary1, real2, imaginary2);
+            return result;
+        }
+        // Method to calculate the subtraction
+        public string Subtraction(int real1, int imaginary1, int real2, int imaginary2)
+        {
+            string result;
+            result = ComplexMath.MathFunctions.Subtract(real1, imaginary1, real2, imaginary2);
+            return result;
+        }
+        // Method to calculate the multiplication
+        public string Multiply(int real1, int imaginary1, int real2, int imaginary2)
+        {
+            string result;
+            result = ComplexMath.MathFunctions.Multiply(real1, imaginary1, real2, imaginary2);
+            return result;
+        }
+        // Method to calculate the division
+        public string Divide(int real1, int imaginary1, int real2, int imaginary2)
+        {
+            string result;
+            result = ComplexMath.MathFunctions.Divide(real1, imaginary1, real2, imaginary2);
+            return result;
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+        ///// Function for calculating the user input 
+        private void btnAdduser_Click(object sender, EventArgs e)
+        {
+            string solution;
+            int real1=0, imaginary1=0, real2=0, imaginary2=0;
+            int.TryParse(txtnum1X.Text, out real1);
+            int.TryParse(txtnum1y.Text, out imaginary1);
+            int.TryParse(txtnum2x.Text, out real2);
+            int.TryParse(txtnum2y.Text, out imaginary2);
+            solution = Addition(real1, imaginary1, real2, imaginary2);
+            lblAnswer.Text = solution.ToString();
+        }
+
+        private void btnSubtractuser_Click(object sender, EventArgs e)
+        {
+            string solution;
+            int real1 = 0, imaginary1 = 0, real2 = 0, imaginary2 = 0;
+            int.TryParse(txtnum1X.Text, out real1);
+            int.TryParse(txtnum1y.Text, out imaginary1);
+            int.TryParse(txtnum2x.Text, out real2);
+            int.TryParse(txtnum2y.Text, out imaginary2);
+            solution = Subtraction(real1, imaginary1, real2, imaginary2);
+            lblAnswer.Text = solution.ToString();
+
+        }
+
+        private void btnMultiplyuser_Click(object sender, EventArgs e)
+        {
+            string solution;
+            int real1 = 0, imaginary1 = 0, real2 = 0, imaginary2 = 0;
+            int.TryParse(txtnum1X.Text, out real1);
+            int.TryParse(txtnum1y.Text, out imaginary1);
+            int.TryParse(txtnum2x.Text, out real2);
+            int.TryParse(txtnum2y.Text, out imaginary2);
+            solution = Multiply(real1, imaginary1, real2, imaginary2);
+            lblAnswer.Text = solution.ToString();
+        }
+
+        private void btnDivideuser_Click(object sender, EventArgs e)
+        {
+            string solution;
+            int real1 = 0, imaginary1 = 0, real2 = 0, imaginary2 = 0;
+            int.TryParse(txtnum1X.Text, out real1);
+            int.TryParse(txtnum1y.Text, out imaginary1);
+            int.TryParse(txtnum2x.Text, out real2);
+            int.TryParse(txtnum2y.Text, out imaginary2);
+            solution = Divide(real1, imaginary1, real2, imaginary2);
+            lblAnswer.Text = solution.ToString();
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+        ///Add the list
+        private void btnAddtxt_Click(object sender, EventArgs e)
+        {
+            //clear the list
+            lstAnswertxt.Items.Clear();
+            string solution;
+            int real1 = 0, imaginary1 = 0, real2 = 0, imaginary2 = 0;
+            //loop through the list
+            for (int i = 0; i<=14; i+=2)
+            {
+                //first number
+                real1 = mylist[i].numX;
+                imaginary1 = mylist[i].numY;
+                //second number
+                real2 = mylist[i+1].numX;
+                imaginary2 = mylist[i+1].numY;
+                // add extra 2 to i so that we go to next 2 numbers
+                
+                solution = Addition(real1, imaginary1, real2, imaginary2);
+                lstAnswertxt.Items.Add(solution);
+                
+
+            }
+        }
+
+        private void btnSubtracttxt_Click(object sender, EventArgs e)
+        {
+            //clear the list
+            lstAnswertxt.Items.Clear();
+            string solution;
+            int real1 = 0, imaginary1 = 0, real2 = 0, imaginary2 = 0;
+            //loop through the list
+            for (int i = 0; i <= 14; i += 2)
+            {
+                //first number
+                real1 = mylist[i].numX;
+                imaginary1 = mylist[i].numY;
+                //second number
+                real2 = mylist[i + 1].numX;
+                imaginary2 = mylist[i + 1].numY;
+                // add extra 2 to i so that we go to next 2 numbers
+
+                solution = Subtraction(real1, imaginary1, real2, imaginary2);
+                lstAnswertxt.Items.Add(solution);
+
+
+            }
+
+        }
+
+        private void btnMultiplytxt_Click(object sender, EventArgs e)
+        {
+            //clear the list
+            lstAnswertxt.Items.Clear();
+            string solution;
+            int real1 = 0, imaginary1 = 0, real2 = 0, imaginary2 = 0;
+            //loop through the list
+            for (int i = 0; i <= 14; i += 2)
+            {
+                //first number
+                real1 = mylist[i].numX;
+                imaginary1 = mylist[i].numY;
+                //second number
+                real2 = mylist[i + 1].numX;
+                imaginary2 = mylist[i + 1].numY;
+                // add extra 2 to i so that we go to next 2 numbers
+
+                solution = Multiply(real1, imaginary1, real2, imaginary2);
+                lstAnswertxt.Items.Add(solution);
+
+            }
+        }
+
+        private void btnDividetxt_Click(object sender, EventArgs e)
+        {
+            //clear the list
+            lstAnswertxt.Items.Clear();
+            string solution;
+            int real1 = 0, imaginary1 = 0, real2 = 0, imaginary2 = 0;
+            //loop through the list
+            for (int i = 0; i <= 14; i += 2)
+            {
+                //first number
+                real1 = mylist[i].numX;
+                imaginary1 = mylist[i].numY;
+                //second number
+                real2 = mylist[i + 1].numX;
+                imaginary2 = mylist[i + 1].numY;
+                // add extra 2 to i so that we go to next 2 numbers
+
+                solution = Divide(real1, imaginary1, real2, imaginary2);
+                lstAnswertxt.Items.Add(solution);
+
+            }
+        }
+    }
+
+}
